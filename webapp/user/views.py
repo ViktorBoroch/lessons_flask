b4a37688248e4c4e4def9608bd6acadb29e1469a
Viktor Boroch <viktorboroch28@gmail.com>
2019-11-06 13:36:47 +0500

    + edit user/views

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -12,9 +12,9 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        flash('Вы уже авторизованы')
+        # flash('Вы уже авторизованы')
         return redirect(get_redirect_target())
-    print(current_user)
+    # print(current_user)
     title = 'Авторизация'
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
